810ae87a4d7fcfe5ecb9f1064a1059a5227f5f2e
dzenannn <dzenan.dev@gmail.com>
2025-04-12 15:39:01 +0200

    form validation

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -3,10 +3,21 @@ $config = require('./database/config.php');
 $db = new Database($config);
 
 if($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $db->query('INSERT INTO notes(body, user_id) VALUES(:body, :user_id)', [
-        'body' => $_POST['body'],
-        'user_id' => 1
-    ]);
+    $errors = [];
+    if(strlen($_POST['body']) === 0) {
+        $errors['body'] = 'A body is required.';
+    }
+
+    if(strlen($_POST['body']) > 600) {
+        $errors['body'] = 'The body can not be more than 600 characters.';
+    }
+
+    if(empty($errors)) {
+        $db->query('INSERT INTO notes(body, user_id) VALUES(:body, :user_id)', [
+            'body' => $_POST['body'],
+            'user_id' => 1
+        ]);
+    }
 }
 
 require 'views/note-create.view.php';
